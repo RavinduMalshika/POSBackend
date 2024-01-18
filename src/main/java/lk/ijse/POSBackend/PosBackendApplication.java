04667b59e0b54ad83454c5fd9f2759bb6fb510a3
RavinduMalshika <ravindu.malshika@gmail.com>
2024-01-18 10:25:14 +0530

    first commit

---
+++ src/main/java/lk/ijse/POSBackend/PosBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.POSBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosBackendApplication.class, args);
+	}
+
+}
